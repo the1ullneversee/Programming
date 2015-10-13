b1c0e9879379b9d9ef0d09c3e0bb89033d7ff117
Thomas Knowles <tkk100@msn.com>
2015-10-13 19:19:51 +0100

    Added skeleton program to be filled in

---
+++ FunctionsAndUserInputPart2/FunctionsAndUserInputPart2/FunctionsAndUserInputPart2.cpp
@@ -0,0 +1,21 @@
+// FunctionsAndUserInputPart2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdarg.h>
+#include <string>
+#include <iostream>
+//<summary> Make this application ask the user for their name, then their age, then print both of these values. Function declarations already put below </summary> // 
+// Remember: TYPE > (the name of the function) > what the function expects as input()
+std::string getName();
+std::size_t getAge();
+void printValues(std::string name, std::size_t age);
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	//CAll functions in here
+	//>> needs something infront of function call >>  getName();
+
+	return 0;
+}
+//Do the functionality for the functions here.

1f46d6533d2f7eeef23ef057c3ae7185ed0638c7
The Architect <thomaskn@DW7RDC845-9.quantel.com>
2015-10-19 17:08:28 +0100

    Added new task, data structures

---
+++ Data_Structures/Data_Structures/Data_Structures.cpp
@@ -0,0 +1,14 @@
+// Data_Structures.cpp : Defines the entry point for the console application.
+// ************************************************************************************************************//
+//<summary> Create an application which creates a data structure called Person with at least 5 different members
+// And fills these different memebers with user input, and displays it back to screen </summary>
+// ***********************************************************************************************************//
+#include "stdafx.h"
+#include <stdio.h>
+#include <string>
+#include <iostream>
+
+int main()
+{
+	return 0;
+}

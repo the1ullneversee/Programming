6221a2a0e20943a1f81ff62fb2c9b7d1c897e97d
The Architect <thomaskn@DW7RDC845-9.quantel.com>
2015-10-19 16:29:44 +0100

    Added new DMA.sln

---
+++ Dynamic_memory_allocation/Dynamic_memory_allocation/Dynamic_memory_allocation.cpp
@@ -0,0 +1,18 @@
+// Dynamic_memory_allocation.cpp : Defines the entry point for the console application.
+//<summary> Create an application that uses Dynamic Memory allocation to let the user enter in any amount of numbers they want, then any number of names</summary>
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <iostream>
+#include <string>
+int main()
+{
+	//Step 1: Declare variables that will hold the user input of element amount.
+	//Step 2: Ask user and gather their input.
+	//Step 3: Using the new operator, dynamically allocate an array and assign it to a pointer
+	//Step 4: Loop through array and get the user to input data into it
+	//Step 5: Let the user see what they have inputted into the container. 
+	//Step 6: Repeat for array of strings. 
+    return 0;
+}
+
